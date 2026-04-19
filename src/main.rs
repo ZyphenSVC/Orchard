bf272f0c7bf40f5b2557f73cc2a77691fd92d225
Sriaditya Vedantam <svedantam@zyphensvc.com>
2026-04-19 13:16:56 -0400

    API backend init

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@
 //     println!("Hello, world!");
 // }
 
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn home() -> impl Responder {
     "Backend is running"
+}
+
+#[get("/heartbeat")]
+async fn heartbeat() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({ "status": "ok" }))
+}
+
+fn api_scope() -> actix_web::Scope {
+    web::scope("/api")
+    .service(heartbeat)
 }
 
 #[actix_web::main]
@@ -15,6 +25,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(home)
+            .service(api_scope())
     })
         .bind("127.0.0.1:3001")?
         .run()
