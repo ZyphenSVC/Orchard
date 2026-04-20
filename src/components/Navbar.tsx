9dbfcd5913e07c14d0f085c74fb95aac9f59d4ed
Sriaditya Vedantam <svedantam@zyphensvc.com>
2026-04-19 21:04:03 -0400

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="border-b px-8 py-4">
+            <div className="flex gap-6">
+                <Link href="/" className="font-medium hover:underline">
+                    Home
+                </Link>
+                <Link href="/music" className="font-medium hover:underline">
+                    Music
+                </Link>
+            </div>
+        </nav>
+    )
+}
