76cb91217ba2f2478b26e848841173ea7f1bfdcb
Sriaditya Vedantam <svedantam@zyphensvc.com>
2026-04-20 04:23:27 -0400

    save progress

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,10 @@
-import { searchMusic } from "@/api/api";
+import SearchClient from "@/app/search/SearchClient";
 
 export default async function SearchPage() {
-    const result = await searchMusic("tefuashfdasuidhas8dh");
-
     return (
         <main className="p-8">
             <h1 className="text-2xl font-bold">Search</h1>
-            <p className="mt-4">Message: {result.message}</p>
-            <p className="mt-2">Value: {result.value}</p>
+            <SearchClient />
         </main>
     );
 }
