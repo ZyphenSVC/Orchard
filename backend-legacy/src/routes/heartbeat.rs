3e8961c40599151c86a0c71b170d91c8c42cd75b
Sriaditya Vedantam <sriaditya.vedantam@gmail.com>
2026-04-23 13:44:29 -0400

    Reset main to empty slate

--- backend-legacy/src/routes/heartbeat.rs
+++
@@ -1,8 +0,0 @@
-use axum::Json;
-use crate::models::api::HeartBeatResponse;
-
-pub async fn heartbeat() -> Json<HeartBeatResponse> {
-    Json(HeartBeatResponse {
-        status: "ok".to_string(),
-    })
-}
